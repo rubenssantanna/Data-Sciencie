3a9f29eab3243c608bc5458ccac323f1c0881bc9
Rubens Sant Anna Junior, Ph.D <106205039+rubenssantanna@users.noreply.github.com>
2023-11-19 14:37:52 -0300

    Update APPDOENÇA2.py

--- APPDOENÇA2.py
+++ APPDOENÇA2.py
@@ -1,10 +1,6 @@
 from re import X
 import streamlit as st 
 from PIL import Image
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
-from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import numpy as np
 import glob
